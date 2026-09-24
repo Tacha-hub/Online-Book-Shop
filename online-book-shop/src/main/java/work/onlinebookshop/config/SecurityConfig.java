c0627d4359609953c2150a1961205e39416839dd
Anastasiia Volichenko <anastasiia.volichenko@gmail.com>
2026-09-24 03:44:47 +0100

    fixed code after comment 02

--- online-book-shop/src/main/java/work/onlinebookshop/config/SecurityConfig.java
+++ online-book-shop/src/main/java/work/onlinebookshop/config/SecurityConfig.java
@@ -29,9 +29,10 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                antMatcher("/auth/**"),
-                                antMatcher("/swagger-ui/**"),
-                                antMatcher("/v3/api-docs/**"))
+                                "/auth/registration",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**")
                         .permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
